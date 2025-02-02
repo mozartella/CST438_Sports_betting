4a075f1f2721010e57955957b1d2a67fecaf5d02
tbear1 <bear.tj21@gmail.com>
2025-02-02 07:40:14 -0800

    added create account page

---
+++ app/navagation/types.ts
@@ -0,0 +1,6 @@
+// navigation/types.ts
+export type RootStackParamList = {
+    Home: undefined; // This is the Index screen
+    CreateAccount: undefined; // This is the AccountCreation screen
+  };
+  
