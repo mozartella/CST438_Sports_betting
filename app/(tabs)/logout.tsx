87d6db35ea86c6f247de262da1a4166ceb5f39e2
athena217 <167275211+athena217@users.noreply.github.com>
2025-10-15 23:06:52 -0700

    HomePage
    Updated the homepage layout to improve visual design and consistency
    Refined the logout screen with better styling and clearer messaging
    Added icons to the bottom tab bar for easier navigation and a more polished look

--- app/(tabs)/logout.tsx
+++ app/(tabs)/logout.tsx
@@ -4,6 +4,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../navagation/types";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 // Define the type for navigation in LogoutScreen
 type LogoutScreenNavigationProp = StackNavigationProp<RootStackParamList, "Logout">;
@@ -25,8 +26,23 @@ const LogoutScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Are you sure you want to log out?</Text>
-      <Button title="Logout" onPress={handleLogout} color="red" />
+      <Ionicons
+        name="log-out-outline"
+        size={120}
+        color="#FF3B30"
+        style={styles.watermarkIcon}
+      />
+      <View style={styles.banner}>
+        <Ionicons
+          name="log-out-outline"
+          size={40}
+          color="#FF3B30"
+          />
+          <Text style={styles.bannerText}>Leaving Your Account</Text>
+      </View>
+
+      <Text style={styles.text}>You're About to Sign Out. Ready to Continue?</Text>
+      <Button title="Logout" onPress={handleLogout} color="#FF3B30" />
     </View>
   );
 };
@@ -36,11 +52,54 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "rgba(155, 216, 243, 1)",
+    paddingHorizontal: 16,
+    position: "relative",
+  },
+  watermarkIcon: {
+    position: "absolute",
+    top: "100%",
+    right: "20%",
+  },
+  banner: {
+    flexDirection: "row",
+    alignItems: "center",
     backgroundColor: "#fff",
+    padding: 12,
+    borderRadius: 12,
+    marginTop: 20,
+    marginBottom: 24,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    elevation: 3,
+  },
+  bannerText: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#25292e",
+    marginLeft: 12,
   },
   text: {
-    fontSize: 18,
-    marginBottom: 20,
+    fontSize: 20,
+    marginBottom: 24,
+    color: "#25292e",
+    textAlign: "center",
+    fontWeight: "600",
+    paddingHorizontal: 24,
+    paddingVertical: 20,
+    backgroundColor: "#fff",
+    borderRadius: 12,
+    width: "80%",
+    shadowColor: "#000",
+    shadowOpacity: 0.05,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  buttonWrapper: {
+    width: "60%",
+    borderRadius: 8,
+    overflow: "hidden",
   },
 });
 
